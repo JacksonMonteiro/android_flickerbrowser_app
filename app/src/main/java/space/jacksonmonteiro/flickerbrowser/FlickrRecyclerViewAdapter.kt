e4baf1bb056c05d427a928d0866c6ee4b806330d
jacksonmonteiro <infor.jackson324@gmail.com>
2022-12-13 10:50:54 -0300

    Recyclerview adapter

---
+++ app/src/main/java/space/jacksonmonteiro/flickerbrowser/FlickrRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package space.jacksonmonteiro.flickerbrowser
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+//
+// Created by Jackson Monteiro on 13/12/2022
+//
+
+class FlickrRecyclerViewAdapter(private var photoList : List<Photo>) : RecyclerView.Adapter<FlickrImageViewHolder>() {
+    private val TAG = "FlickrRecyclerViewAdapt"
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickrImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.browse, parent, false)
+        return FlickrImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return if (photoList.isNotEmpty()) photoList.size else 0
+    }
+
+    fun loadNewData(newPhotos: List<Photo>){
+        photoList = newPhotos
+        notifyDataSetChanged()
+    }
+
+    fun getPhoto(position: Int) : Photo? {
+        return if (photoList.isNotEmpty()) photoList[position] else null
+    }
+}
+
+class FlickrImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var thumbnail:  ImageView = view.findViewById(R.id.thumbnail)
+    var title: TextView = view.findViewById(R.id.photo_title)
+}
