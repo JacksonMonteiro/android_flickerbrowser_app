b30b6d06a4ac3e42de5b2ee9fa65dd0d5f221373
jacksonmonteiro <infor.jackson324@gmail.com>
2022-12-13 09:20:21 -0300

    Search and Photo Details Activities

---
+++ app/src/main/java/space/jacksonmonteiro/flickerbrowser/PhotoDetailsActivity.kt
@@ -0,0 +1,30 @@
+package space.jacksonmonteiro.flickerbrowser
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import space.jacksonmonteiro.flickerbrowser.databinding.ActivityPhotoDetailsBinding
+
+class PhotoDetailsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityPhotoDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPhotoDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
---
+++ app/src/main/java/space/jacksonmonteiro/flickerbrowser/SearchActivity.kt
@@ -0,0 +1,26 @@
+package space.jacksonmonteiro.flickerbrowser
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import space.jacksonmonteiro.flickerbrowser.databinding.ActivitySearchBinding
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
