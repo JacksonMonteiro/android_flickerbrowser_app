ebc3fc6346f68fd7b1d382270c1752491d2185fb
jacksonmonteiro <infor.jackson324@gmail.com>
2022-12-14 10:52:35 -0300

    PhotoDetailsActivity

--- app/src/main/java/space/jacksonmonteiro/flickerbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/space/jacksonmonteiro/flickerbrowser/PhotoDetailsActivity.kt
@@ -1,14 +1,22 @@
 package space.jacksonmonteiro.flickerbrowser
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.squareup.picasso.Picasso
+import org.w3c.dom.Text
 import space.jacksonmonteiro.flickerbrowser.databinding.ActivityPhotoDetailsBinding
 
 class PhotoDetailsActivity : BaseActivity() {
-
     private lateinit var binding: ActivityPhotoDetailsBinding
+
+    private lateinit var photoTitle: TextView
+    private lateinit var photoAuthor: TextView
+    private lateinit var photoTags: TextView
+    private lateinit var photoItem: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,19 @@ class PhotoDetailsActivity : BaseActivity() {
         setContentView(binding.root)
         activateToolbar(true)
 
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        photoTitle = findViewById(R.id.photo_title)
+        photoAuthor = findViewById(R.id.photo_author)
+        photoTags = findViewById(R.id.photo_tags)
+        photoItem = findViewById(R.id.photo_image)
 
+        photoTitle.text = photo.title
+        photoAuthor.text = photo.author
+        photoTags.text = photo.tags
+
+        Picasso.get().load(photo.link)
+            .error(R.drawable.ic_placeholder)
+            .placeholder(R.drawable.ic_placeholder)
+            .into(photoItem)
     }
 }
